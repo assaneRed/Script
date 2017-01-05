7e0d270d2442ebe29488c2c56a3784b82b497153
Nicolas ROBIDEL <nrobidel@redtechnologies.fr>
2017-01-05 10:29:36 +0100

    05/01: Ajout de fonctions, modification des paramètres de completion d'array en objets, problème de concatenation résolu.

--- Syncthing/ScriptSyncthing.ps1
+++ Syncthing/ScriptSyncthing.ps1
@@ -2,11 +2,6 @@
 #Version 1.0
 #Auteur: Nicolas ROBIDEL
 
-cls
-
-#Type d'éxecutioon
-Set-ExecutionPolicy Unrestricted
-
 #Ignore le certificat SSL
 Add-Type @"
     using System;
@@ -34,36 +29,33 @@ Add-Type @"
 
 [ServerCertificateValidationCallback]::Ignore();
 
-#Variables nécessaires, compteur, arrays et string 
-$compteurErreurs=0
-$compteur=0
-$devices = "W2Y7VJS-4BZNCSV-3E5WGJV-HKDU2PQ-LXI2YQL-M6YVQ6K-EDCFI2T-FZR2GQ6", "YB355HA-PW5XWDP-R37EHXH-NQOYKSQ-2G5NCXS-GGGM2H2-AE5MSAP-LFI6SAU", "PGF4U2Z-V7AD7KP-TMUU2P5-3UVG7BH-PD3XPIS-DJ5H54I-NADG2GT-BXZG2QU"
-$foldersGit = "gitrepositories", "redmineFiles", "redmineDb", "SharedDisk"
+#Déclaration de variables
+$compteurErreursConnexions=0
+$compteurErreursCompletion=0
 $message = ""
 $messageCompletion = ""
+$messageMail=""
+$IPs = "192.168.1.32", "192.168.1.2", "37.187.25.79"
 
-#Fonction envoi de mail
-function Mail
+#Paramètres pour getCompletion (Objets à 3 propriétés : ip du serveur de d'origine, id du serveur de destination, id du folder) 
+$compGitNas_redmineFiles = New-Object PSObject -Property @{IP="192.168.1.32"; DeviceID="YB355HA-PW5XWDP-R37EHXH-NQOYKSQ-2G5NCXS-GGGM2H2-AE5MSAP-LFI6SAU"; Folder="redmineFiles"}
+$compGitNas_redmineDb = New-Object PSObject -Property @{IP="192.168.1.32"; DeviceID="YB355HA-PW5XWDP-R37EHXH-NQOYKSQ-2G5NCXS-GGGM2H2-AE5MSAP-LFI6SAU"; Folder="redmineDb"}
+$compGitNas_gitrepo = New-Object PSObject -Property @{IP="192.168.1.32"; DeviceID="YB355HA-PW5XWDP-R37EHXH-NQOYKSQ-2G5NCXS-GGGM2H2-AE5MSAP-LFI6SAU"; Folder="gitrepositories"}
+$compNasOvh_gitrepo = New-Object PSObject -Property @{IP="192.168.1.2"; DeviceID="PGF4U2Z-V7AD7KP-TMUU2P5-3UVG7BH-PD3XPIS-DJ5H54I-NADG2GT-BXZG2QU"; Folder="gitrepositories"}
+$compNasOvh_sharedDisk = New-Object PSObject -Property @{IP="192.168.1.2"; DeviceID="PGF4U2Z-V7AD7KP-TMUU2P5-3UVG7BH-PD3XPIS-DJ5H54I-NADG2GT-BXZG2QU"; Folder="SharedDisk"}
+$completion = $compGitNas_redmineFiles, $compGitNas_redmineDb, $compGitNas_gitrepo, $compNasOvh_gitrepo, $compNasOvh_sharedDisk
+
+#Fonction envoi de mail prend 2 paramètres (string) le sujet du mail et le message
+function mail
 {
-    param($message)
-    $smtpServer = "smtp.orange.fr"
-    $from = "SyncThing <nrobidel@redtechnologies.fr>"
+    param($subject, $messageMail)
+    $smtpServer = "ssl0.ovh.net"
+    $pwd = ConvertTo-SecureString "newlsa2300" -AsPlainText -Force
+    $cred = New-Object Management.Automation.PSCredential ('nrobidel@redtechnologies.fr',$pwd)
+    $from = "Syncthing <nrobidel@redtechnologies.fr>"
     $to = "Nicolas ROBIDEL <nrobidel@redtechnologies.fr>"
-    $subject = "Test d'envoi de mail"
-    $body = "
-    <html>
-      <head></head>
-         <body>
-            <p>Bonjour,<br /><br />
-               Ceci est un test, no need to panic ! <br />
-               '$message'<br />
-               <br />
-               Cordialement, message systeme. 
-            </p>
-          </body>
-    </html>"
-
-    Send-MailMessage -smtpserver $smtpserver -from $from -to $to -subject $subject -body $body -bodyasHTML -priority High
+    $body = "Bonjour,`n`n$messageMail `n`nCordialement, message systeme."
+    Send-MailMessage -smtpserver $smtpserver -from $from -to $to -subject $subject -body $body -Credential $cred -priority High
 }
 
 # Fonction permettant de récupérer la liste des connections et la date des connections (.../rest/system/connections)
@@ -75,7 +67,6 @@ function getConnections
     $pwd = ConvertTo-SecureString "lsa@2300" -AsPlainText -Force
     $cred = New-Object Management.Automation.PSCredential ('red',$pwd)
     $url = "https://"+$ip+":8384/rest/system/connections"
-
     Invoke-WebRequest -Uri $url -Method Get -Credential $cred | Format-List "content"
     
 }
@@ -87,88 +78,125 @@ function getConnections
 #     - l'ID du dossier
 function getCompletion
 {
-  param ([string]$ip, [string]$deviceID, [string]$folder)
+    param ($param)
     $pwd = ConvertTo-SecureString "lsa@2300" -AsPlainText -Force
     $cred = New-Object Management.Automation.PSCredential ('red',$pwd)
-    $url = "https://"+$ip+":8384/rest/db/completion?device="+$deviceID+"&folder="+$folder
-    echo $ip
+    $url = "https://"+$param.IP+":8384/rest/db/completion?device="+$param.DeviceID+"&folder="+$param.Folder
     Invoke-WebRequest -Uri $url -Method Get -Credential $cred | Format-List "content"
 }
 
-#Variables contenant les résultats de la fonction getConnections pour les serveurs GIT, NAS et OVH
-#Out-String transforme le résultat en string
-$connecGIT = getConnections "192.168.1.32" | Out-String
-$connecNAS = getConnections "192.168.1.2"  | Out-String
-$connecOVH = getConnections "37.187.25.79" | Out-String
+#Prend en paramètre l'ID d'un device et retourne un string contenant le nom du device
+function getdeviceNameFromId
+{
+    param($id)
+    switch($id)
+    {
+        "W2Y7VJS-4BZNCSV-3E5WGJV-HKDU2PQ-LXI2YQL-M6YVQ6K-EDCFI2T-FZR2GQ6"{return "GIT"}
+        "YB355HA-PW5XWDP-R37EHXH-NQOYKSQ-2G5NCXS-GGGM2H2-AE5MSAP-LFI6SAU"{return "NAS"}
+        "PGF4U2Z-V7AD7KP-TMUU2P5-3UVG7BH-PD3XPIS-DJ5H54I-NADG2GT-BXZG2QU"{return "OVH"}
+        default{"Aucune correspondance trouvée"}
+    }
+}
 
-#Variables contenant le résultat du test de completion
+#Prend en paramètre l'IP d'un device et retourne un string contenant le nom du device
+function getDeviceNameFromIp
+{
+    param($ip)
+    switch($ip)
+    {
+        "192.168.1.32"{return "GIT"}
+        "192.168.1.2"{return "NAS"}
+        "37.187.25.79"{return "OVH"}
+        default{"Aucune correspondance trouvée"}
+    }
+}
 
-$compGitNas_redmineFiles = getCompletion "192.168.1.32" "YB355HA-PW5XWDP-R37EHXH-NQOYKSQ-2G5NCXS-GGGM2H2-AE5MSAP-LFI6SAU" "redmineFiles" | Out-String
-$compGitNas_redmineDb = getCompletion "192.168.1.32" "YB355HA-PW5XWDP-R37EHXH-NQOYKSQ-2G5NCXS-GGGM2H2-AE5MSAP-LFI6SAU" "redmineDb" | Out-String
-$compGitNas_gitrepo = getCompletion "192.168.1.32" "YB355HA-PW5XWDP-R37EHXH-NQOYKSQ-2G5NCXS-GGGM2H2-AE5MSAP-LFI6SAU" "gitrepositories" | Out-String
-$compNasOvh_gitrepo = getCompletion "192.168.1.2" "PGF4U2Z-V7AD7KP-TMUU2P5-3UVG7BH-PD3XPIS-DJ5H54I-NADG2GT-BXZG2QU" "gitrepositories" | Out-String
-$compNasOvh_sharedDisk = getCompletion "192.168.1.2" "PGF4U2Z-V7AD7KP-TMUU2P5-3UVG7BH-PD3XPIS-DJ5H54I-NADG2GT-BXZG2QU" "SharedDisk" | Out-String
-$completion = $compGitNas_redmineFiles, $compGitNas_redmineDb, $compGitNas_gitrepo, $compNasOvh_gitrepo, $compNasOvh_sharedDisk
-
-#Compte le nombre d'apparitions du mot "true" dans le résultat de getConnections et le stock dans une variable
+#Fonction prenant en paramètre un array d'adresses IP et utilise la fonction getConnection pour tester les connexions des éléments du tableau
 #Cela indique si les serveurs sont bien connectés (GIT et OVH doivent valoir 1 et le NAS quand a lui 2)
-$countTrueGit =([regex]::Matches($connecGIT, "true" )).count
-$countTrueNas =([regex]::Matches($connecNAS, "true" )).count
-$countTrueOvh =([regex]::Matches($connecOVH, "true" )).count
-
-if($countTrueGit -ne 1 -or $countTrueNas -ne 2 -or $countTrueOvh -ne 1)
+function testConnexion
 {
-    echo "Une ou plusieurs erreurs de connexions rencontrés !"
-    if ($countTrueGit -ne 1)
+    param($connexion)
+    foreach($element in $connexion)
     {
-        $compteurErreurs += 1
-        $message += "ERREUR ! Il semblerait que le service SyncThing du serveur GIT ne soit pas connecté au serveur NAS.`n"
-    }
-    if ($countTrueNAS -ne 2)
-    {
-        if ($countTrueNAS -eq 0)
+        $result = getConnections $element | Out-String
+        $serveur = getDeviceNameFromIp $element
+        switch($element)
         {
-            $compteurErreurs += 1
-            $message += "ERREUR ! Il semblerait que le service SyncThing du serveur NAS ne soit pas connecté au serveur GIT ni au serveur OVH.`n"
+            "192.168.1.2"
+            {
+                switch(([regex]::Matches($result, "true" )).count)
+                {
+                    2 {}
+                    default
+                    {
+                       $script:compteurErreursConnexions += 1
+                       $message += " - Probleme de connexion de SyncThing sur le serveur "+$serveur+".`n" 
+                    }
+                }
+            }
+            default
+            {
+                if(([regex]::Matches($result, "true" )).count -ne 1)
+                {
+                    $script:compteurErreursConnexions += 1
+                    $message += " - Probleme de connexion de SyncThing sur le serveur "+$serveur+".`n"
+                }
+            }
         }
-        else
-        {
-            $compteurErreurs += 1
-            $message += "ERREUR ! Il semblerait que le service SyncThing du serveur NAS ne soit pas connecté au serveur GIT ou au serveur OVH.`n"
-        }   
-    }
-    if ($countTrueGit -ne 1)
-    {
-        $compteurErreurs += 1
-        $message += "ERREUR ! Il semblerait que le service SyncThing du serveur OVH ne soit pas connecté au serveur NAS.`n"
     }
 
-}
-else
-{
-    $message += "Aucune erreur détecté, tous les services semblent fonctionné normalement !`n"
-}
-
-foreach($result in $completion)
-{
-    if(([regex]::Matches($result, 100 )).count -ne 1)
+    if($compteurErreursConnexions -eq 0)
     {
-        $compteurErreurs+=1
-        switch($compteur)
+        $script:messageMail += "Aucune erreur de connexion rencontree lors des tests, tout semble OK.`n"
+    }
+    else
+    { 
+        $script:messageMail += "ERREUR, $compteurErreursConnexions erreur(s) rencontree(s) lors des tests de connexions.`n"
+    }
+    $script:messageMail += $message
+    }
+
+#Fonction prenant en paramètre un array contenant des objets paramètres ("ip","device id", "folder id") et vérifie l'etat de la synchronisation
+function testCompletion
+{
+    param($completion)
+    foreach($element in $completion)
+    {
+        $result = getCompletion $element | Out-String
+        $source = getDeviceNameFromIp $element.IP
+        $destination = getDeviceNameFromId $element.DeviceID
+        if(([regex]::Matches($result,100 )).count -ne 1)
         {
-            0 {$messageCompletion += "Il semblerait qu'il y ai eu un problème de synchronisation du dossier 'redmineDB' du serveur GIT au serveur NAS"}
-            1 {$messageCompletion += "Il semblerait qu'il y ai eu un problème de synchronisation du dossier 'redmineFiles' du serveur GIT au serveur NAS"}
-            2 {$messageCompletion += "Il semblerait qu'il y ai eu un problème de synchronisation du dossier 'gitrepositories' du serveur GIT au serveur NAS"}
-            3 {$messageCompletion += "Il semblerait qu'il y ai eu un problème de synchronisation du dossier 'gitrepositories' du serveur NAS au serveur OVH"}
-            4 {$messageCompletion += "Il semblerait qu'il y ai eu un problème de synchronisation du dossier 'SharedDisk' du serveur NAS au serveur OVH"}
+            $script:compteurErreursCompletion+=1
+            $messageCompletion += " - Probleme de synchronisation du dossier "+$element.Folder+" du serveur "+$source+" vers le serveur "+$destination+".`n"
+        
         }
-
     }
-    $compteur +=1
+    if($compteurErreursCompletion -eq 0)
+    {
+        $script:messageMail += "Aucune erreur de synchronisation rencontree lors des tests, tout semble OK.`n"
+    }
+    else
+    {
+        $script:messageMail += "ERREUR, $compteurErreursCompletion erreur(s) rencontree(s) lors des tests de synchronisation.`n"
+    }
+    $script:messageMail += $messageCompletion
 }
 
-#Envoi de mail signalant le nombre d'erreurs rencontrés ainsi que plus de détails concernant les erreurs en question (si elles existent)
-#Mail "Nous avons rencontrés $compteurErreurs erreurs.`n$message"
+#Fonctions lançant les différents tests et envoyant le mail de rapport a l'admin en utilisant les fonctions mail, testConnexion et testCompletion
+function main
+{
+    testConnexion $IPs
+    testCompletion $completion
+    if ($compteurErreursConnexions -ne 0 -or $compteurErreursCompletion -ne 0)
+    {
+        mail "ERREUR ! Rapport Syncthing" $messageMail
+    }
+    else
+    {
+        mail "OK ! Rapport Syncthing" $messageMail
+    }
+    echo $messageMail
+}
 
-echo "Nous avons rencontrés $compteurErreurs erreurs."
-echo $message
+main
