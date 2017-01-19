25292010702ae96b330c4e36dd6381592c7bcd90
Nicolas ROBIDEL <nrobidel@redtechnologies.fr>
2017-01-19 10:41:35 +0100

    Adding KeyGen script

---
+++ KeyGen/KeyGen.ps1
@@ -0,0 +1,7 @@
+﻿$key = New-Object byte[](32)
+$rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::Create()
+$rng.GetBytes($key)
+$key | Out-File key.txt
+
+$pwd = Read-Host -AsSecureString "Enter a secret password." | ConvertFrom-SecureString -Key $key
+$pwd | Out-file securePass.txt
